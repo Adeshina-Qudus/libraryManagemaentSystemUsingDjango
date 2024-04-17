5602c76ce25f8a91a7eb03aaf0068d13b9b490e4
Adeshina Qudus <qudusa55@gmail.com>
2024-04-17 11:45:57 +0100

    serializer

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -2,3 +2,6 @@ from rest_framework import serializers
 
 
 class BookSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=225)
+    summary = serializers.CharField(max_length=1000)
+    isbn = serializers.CharField(max_length=20)
--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Book
+from .serializers import BookSerializer
 
 
 # Create your views here.
 
 @api_view()
 def book_list(request):
-
-    return Response('books')
+    book = Book.objects.all()
+    serializer = BookSerializer(book, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @api_view()
 def book_details(request, id):
-    return Response(id)
+    try:
+        book = Book.objects.get(id=id)
+        serializer = BookSerializer(book)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except Book.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
