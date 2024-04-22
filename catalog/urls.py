4e1773a9b53e85586dfacad5bd9158c28f60bd24
Adeshina Qudus <qudusa55@gmail.com>
2024-04-22 09:33:44 +0100

    refactor

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.book_list),
+    path('books/', views.BookList.as_view(), name='books'),
     path('books/<int:pk>', views.book_details, name='book_details'),
     path('author', views.author_list),
     # path('author/)
